45ca81d7e953c47e265a94898f1ae841391a0bbc
VidushaSanidu <vidushsanidu07@gmail.com>
2025-07-22 13:18:32 +0530

    minor

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "../server/routes.js";
-import { serveStatic } from "../server/vite.js";
+// import { serveStatic } from "../server/vite.js";
 
 const app = express();
 app.use(express.json());
